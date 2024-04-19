ec3b0cbc485f46ece4bffe1b918b59bfb7b7d0ed
Your Name <you@example.com>
2024-04-19 13:41:04 +0200

    Fikset billett tabell etter etternavn

--- src/main/java/com/example/oblig3_1/BillettRepository.java
+++ src/main/java/com/example/oblig3_1/BillettRepository.java
@@ -40,7 +40,7 @@ public class BillettRepository {
 
 
     public List<Billett> hentAlleBilletter(){
-        String sql = "SELECT * FROM Billett";
+        String sql = "SELECT * FROM Billett ORDER BY etternavn ASC";
         try {
             List<Billett> alleBilletter = db.query(sql, new BeanPropertyRowMapper(Billett.class));
             return alleBilletter;
